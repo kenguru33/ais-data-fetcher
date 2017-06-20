440ed1fc9c400cbd3a0587294f63a2ec8b8605e9
Bernt Anker <bernt.anker@me.com>
2017-06-20 09:29:26 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const aisDataFetcher = require('./lib/kystverket/index')
-
+console.log("starting  fetching from " + process.env.URL)
 setInterval(() => {
   aisDataFetcher(process.env.URL)
     .then(data => {
